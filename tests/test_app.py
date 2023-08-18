d709984ef6fb26cdd9c6eb4507211e4e3f9b5f62
Ostap Ohorodnyk <95187662+ohorodnykostap@users.noreply.github.com>
2023-08-18 18:05:21 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,17 @@
-from app import app
+# tests/test_db.py
 
-def test_homepage():
-    client = app.test_client()
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b"Welcome to the App" in response.data
+import pytest
+from db import db, ProductModel, create_product
 
+@pytest.fixture(scope="module")
+def setup_database():
+    db.connect()
+    db.create_tables([ProductModel])
+    yield
+    db.drop_tables([ProductModel])
+    db.close()
+
+def test_create_product(setup_database):
+    product = create_product("Test Product", 10)
+    assert product.name == "Test Product"
+    assert product.price == 10
