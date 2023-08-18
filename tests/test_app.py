c24686dc8a7c52eb030b944dd8129d9e5c46bba1
Ostap Ohorodnyk <95187662+ohorodnykostap@users.noreply.github.com>
2023-08-18 18:32:04 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,62 @@
 import pytest
-from api import app # Flask instance of the API
+import json
+from app import app
+from db import *
 
-def test_index_route():
-    response = app.test_client().get('/')
+def test_get_products():
+    client = app.test_client()
+    response = client.get('/api/products')
+    data = response.get_json()
 
     assert response.status_code == 200
-    assert response.data.decode('utf-8') == 'Testing, Flask!'
+    assert isinstance(data, list)
+
+def test_create_product():
+    client = app.test_client()
+    response = client.post('/api/products', json={'name': 'Test Product', 'price': 10})
+    data = response.get_json()
+
+    assert response.status_code == 201
+    assert 'message' in data
+    assert 'productId' in data
+
+    product_id = data['productId']
+    product = get_product_by_id(product_id)
+    assert product is not None
+    assert product.name == 'Test Product'
+
+def test_get_product_by_id():
+    product = create_product('Test Product', 10)
+    client = app.test_client()
+    response = client.get(f'/api/products/{product.id}')
+    data = response.get_json()
+
+    assert response.status_code == 200
+    assert 'id' in data
+    assert 'name' in data
+    assert 'price' in data
+
+def test_update_product():
+    product = create_product('Test Product', 10)
+    client = app.test_client()
+    response = client.patch(f'/api/products/{product.id}', json={'price': 15})
+    data = response.get_json()
+
+    assert response.status_code == 200
+    assert 'message' in data
+
+    updated_product = get_product_by_id(product.id)
+    assert updated_product.price == 15
+
+def test_delete_product():
+    product = create_product('Test Product', 10)
+    client = app.test_client()
+    response = client.delete(f'/api/products/{product.id}')
+    data = response.get_json()
+
+    assert response.status_code == 200
+    assert 'message' in data
+
+    deleted_product = get_product_by_id(product.id)
+    assert deleted_product is None
+
