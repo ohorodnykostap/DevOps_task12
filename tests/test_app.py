438220b65b3305166b13a6b25879b6ae15c80db5
Ostap Ohorodnyk <95187662+ohorodnykostap@users.noreply.github.com>
2023-08-18 18:39:52 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,16 +3,24 @@ import json
 from app import app
 from db import *
 
-def test_get_products():
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
     client = app.test_client()
+
+    with app.app_context():
+        init_db()
+        yield client
+        clear_db()
+
+def test_get_products(client):
     response = client.get('/api/products')
     data = response.get_json()
 
     assert response.status_code == 200
     assert isinstance(data, list)
 
-def test_create_product():
-    client = app.test_client()
+def test_create_product(client):
     response = client.post('/api/products', json={'name': 'Test Product', 'price': 10})
     data = response.get_json()
 
@@ -25,9 +33,8 @@ def test_create_product():
     assert product is not None
     assert product.name == 'Test Product'
 
-def test_get_product_by_id():
+def test_get_product_by_id(client):
     product = create_product('Test Product', 10)
-    client = app.test_client()
     response = client.get(f'/api/products/{product.id}')
     data = response.get_json()
 
@@ -36,9 +43,8 @@ def test_get_product_by_id():
     assert 'name' in data
     assert 'price' in data
 
-def test_update_product():
+def test_update_product(client):
     product = create_product('Test Product', 10)
-    client = app.test_client()
     response = client.patch(f'/api/products/{product.id}', json={'price': 15})
     data = response.get_json()
 
@@ -48,9 +54,8 @@ def test_update_product():
     updated_product = get_product_by_id(product.id)
     assert updated_product.price == 15
 
-def test_delete_product():
+def test_delete_product(client):
     product = create_product('Test Product', 10)
-    client = app.test_client()
     response = client.delete(f'/api/products/{product.id}')
     data = response.get_json()
 
@@ -59,4 +64,3 @@ def test_delete_product():
 
     deleted_product = get_product_by_id(product.id)
     assert deleted_product is None
-
