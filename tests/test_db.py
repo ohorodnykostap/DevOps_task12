4f7ef11997245b0892ac900b5d8b2a41afe124e0
Ostap Ohorodnyk <95187662+ohorodnykostap@users.noreply.github.com>
2023-08-18 18:33:05 +0300

    Update test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,45 +2,36 @@ import pytest
 from peewee import OperationalError, SqliteDatabase
 from db import *
 
-# Решта вашого коду для тестів...
-
-
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope='module')
 def test_db():
-    db = SqliteDatabase(':memory:')  # Використовуємо пам'ять як базу даних для тестів
-    ProductModel._meta.database = db
     db.connect()
-    yield
-    db.close()
+    db.create_tables([ProductModel])
 
+    yield db
+
+    db.drop_tables([ProductModel])
+    db.close()
 
 def test_create_product(test_db):
     product = create_product("Test Product", 10)
+    assert product is not None
     assert product.id is not None
-
 
 def test_get_product_by_id(test_db):
     product = create_product("Test Product", 10)
     retrieved_product = get_product_by_id(product.id)
     assert retrieved_product is not None
-
+    assert retrieved_product.name == "Test Product"
 
 def test_update_product(test_db):
     product = create_product("Test Product", 10)
-    updated_product = update_product(product.id, name="Updated Product")
+    updated_product = update_product(product.id, price=15)
     assert updated_product is not None
-    assert updated_product.name == "Updated Product"
-
+    assert updated_product.price == 15
 
 def test_delete_product(test_db):
     product = create_product("Test Product", 10)
-    result = delete_product(product.id)
-    assert result is True
-    deleted_product = get_product_by_id(product.id)
-    assert deleted_product is None
-
-
-def test_get_product_by_id_nonexistent(test_db):
-    retrieved_product = get_product_by_id(999)
-    assert retrieved_product is None
+    delete_product(product.id)
+    with pytest.raises(ProductModel.DoesNotExist):
+        get_product_by_id(product.id)
 
