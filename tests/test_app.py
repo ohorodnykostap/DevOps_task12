2c983cf97c86ef41ffc4dd7d206de28045073cbf
Ostap Ohorodnyk <95187662+ohorodnykostap@users.noreply.github.com>
2023-08-18 17:56:26 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,8 @@
-import unittest
 from app import app
-from peewee import SqliteDatabase
-from playhouse.test_utils import test_database
-from db import ProductModel, create_product
 
-# Use an in-memory SQLite database for testing
-test_db = SqliteDatabase(":memory:")
+def test_homepage():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Welcome to the App" in response.data
 
-class TestProductAPI(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-        self.db_context = test_database(test_db, [ProductModel])
-        self.db_context.__enter__()
-        test_db.create_tables([ProductModel])
-    
- 
-if __name__ == "__main__":
-    unittest.main()
