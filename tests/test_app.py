a0dc0fdaecf0f74fca92c4d5eb7811ee3ed4b538
Ostap Ohorodnyk <95187662+ohorodnykostap@users.noreply.github.com>
2023-08-18 17:39:58 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,40 +1,59 @@
 import unittest
 from app import app
+from peewee import SqliteDatabase
+from playhouse.test_utils import test_database
+from db import ProductModel, create_product
 
+# Use an in-memory SQLite database for testing
+test_db = SqliteDatabase(":memory:")
 
-class TestApp(unittest.TestCase):
+class TestProductAPI(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
-
+        self.db_context = test_database(test_db, [ProductModel])
+        self.db_context.__enter__()
+        test_db.create_tables([ProductModel])
+    
+    def tearDown(self):
+        self.db_context.__exit__(None, None, None)
+    
     def test_get_products(self):
-        response = self.app.get('/api/products')
+        response = self.app.get("/api/products")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json), 5)  # Assuming there are 5 default products
+        self.assertEqual(response.get_json(), [])
 
-    def test_get_product_by_id(self):
-        response = self.app.get('/api/products/1')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json['id'], 1)
-        self.assertEqual(response.json['name'], 'Sugar')
-        self.assertEqual(response.json['price'], 32)
-
-    def test_get_nonexistent_product(self):
-        response = self.app.get('/api/products/10')
-        self.assertEqual(response.status_code, 404)
-
-    def test_create_product(self):
-        data = {'name': 'Tea', 'price': 15}
-        response = self.app.post('/api/products', json=data)
+    def test_add_product(self):
+        response = self.app.post("/api/products", json={"name": "Test Product", "price": 10.99})
         self.assertEqual(response.status_code, 201)
-        self.assertIn('productId', response.json)
-
-    def test_update_product(self):
-        data = {'name': 'Updated Product', 'price': 25}
-        response = self.app.patch('/api/products/1', json=data)
+        self.assertIn("productId", response.get_json())
+        
+        product_id = response.get_json()["productId"]
+        product = ProductModel.get_by_id(product_id)
+        self.assertEqual(product.name, "Test Product")
+        self.assertEqual(product.price, 10.99)
+    
+    def test_get_product(self):
+        product = create_product(name="Test Product", price=10.99)
+        response = self.app.get(f"/api/products/{product.id}")
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json()["name"], "Test Product")
+        self.assertEqual(response.get_json()["price"], 10.99)
+    
+    def test_update_product(self):
+        product = create_product(name="Test Product", price=10.99)
+        response = self.app.patch(f"/api/products/{product.id}", json={"name": "Updated Product"})
+        self.assertEqual(response.status_code, 200)
+        
+        updated_product = ProductModel.get_by_id(product.id)
+        self.assertEqual(updated_product.name, "Updated Product")
+    
+    def test_delete_product(self):
+        product = create_product(name="Test Product", price=10.99)
+        response = self.app.delete(f"/api/products/{product.id}")
+        self.assertEqual(response.status_code, 200)
+        
+        with self.assertRaises(ProductModel.DoesNotExist):
+            ProductModel.get_by_id(product.id)
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
